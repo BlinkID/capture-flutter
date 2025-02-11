68f498efe5fc95920ccc3e71f2c0ea92bc418520
Milan Paradina <milan.paradina@microblink.com>
2025-02-11 16:11:53 +0100

    add scanWithCamera to dart method channel

--- capture_flutter/lib/capture_flutter_method_channel.dart
+++ capture_flutter/lib/capture_flutter_method_channel.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-
 import 'capture_flutter_platform_interface.dart';
+import 'CaptureAnalyzerResult.dart';
 
 /// An implementation of [CaptureFlutterPlatform] that uses method channels.
 class MethodChannelCaptureFlutter extends CaptureFlutterPlatform {
   /// The method channel used to interact with the native platform.
   @visibleForTesting
-  final methodChannel = const MethodChannel('capture_flutter');
+  static const methodChannel = const MethodChannel('capture_flutter');
 
-  @override
-  Future<String?> getPlatformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
-    return version;
+  // @override
+  // static Future<String?> getPlatformVersion() async {
+  //const version =
+  //    await methodChannel.invokeMethod<String>('getPlatformVersion');
+//    return "version";
+//  }
+
+  static Future<AnalyzerResult?> scanWithCamera() async {
+    final analyzerResult =
+        await methodChannel.invokeMethod<AnalyzerResult>('scanWithCamera');
+    return analyzerResult;
   }
 }
