dc78960ef1da3f8f44f03af273eba2678777c1e4
Milan Paradina <milan.paradina@microblink.com>
2025-02-11 16:16:36 +0100

    add initial iOS channel

--- capture_flutter/ios/Classes/CaptureFlutterPlugin.swift
+++ capture_flutter/ios/Classes/CaptureFlutterPlugin.swift
@@ -1,7 +1,10 @@
 import Flutter
 import UIKit
+import CaptureUX
 
 public class CaptureFlutterPlugin: NSObject, FlutterPlugin {
+    var captureSettings: MBICCaptureSettings?
+    
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "capture_flutter", binaryMessenger: registrar.messenger())
     let instance = CaptureFlutterPlugin()
@@ -12,8 +15,57 @@ public class CaptureFlutterPlugin: NSObject, FlutterPlugin {
     switch call.method {
     case "getPlatformVersion":
       result("iOS " + UIDevice.current.systemVersion)
+    case "scanWithCamera":
+        scanWithCamera()
     default:
       result(FlutterMethodNotImplemented)
     }
   }
+    
+    public func scanWithCamera() {
+      setupKey()
+      setupCaptureSettings()
+      let captureVC = MBICCaptureViewController()
+      captureVC.delegate = self
+      captureVC.modalPresentationStyle = .fullScreen
+      let rootVc = UIApplication.shared.keyWindow?.rootViewController
+      rootVc?.present(captureVC, animated: true)
+    }
+    
+    private func setupCaptureSettings() {
+        self.captureSettings = MBICCaptureSettings()
+    }
+      
+      private func setupKey() {
+          MBCCCaptureCoreSDK.shared().setLicenseKey("license-key") { captureLicenseKeyError in
+              switch captureLicenseKeyError {
+              case .networkRequired:
+                  print("Network required")
+              case .unableToDoRemoteLicenceCheck:
+                  print("unableToDoRemoteLicenceCheck")
+              case .licenseIsLocked:
+                  print("licenseIsLocked")
+              case .licenseCheckFailed:
+                  print("licenseCheckFailed")
+              case .invalidLicense:
+                  print("invalidLicense")
+              case .permissionExpired:
+                  print("permissionExpired")
+              case .payloadCorrupted:
+                  print("payloadCorrupted")
+              case .payloadSignatureVerificationFailed:
+                  print("payloadSignatureVerificationFailed")
+              case .incorrectTokenState:
+                  print("incorrectTokenState")
+              @unknown default:
+                  print("Unknown license key error")
+              }
+          }
+      }
+}
+
+extension CaptureFlutterPlugin: MBICCaptureViewControllerDelegate {
+    public func captureViewController(captureViewController: MBICCaptureViewController, didFinishCaptureWithResult analyzerResult: MBCCAnalyzerResult) {
+        print(analyzerResult.description)
+    }
 }
