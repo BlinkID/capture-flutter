b411ada557f2e7022642a05227430c0f25c599f7
Milan Paradina <milan.paradina@microblink.com>
2025-02-12 10:57:27 +0100

    update main.dart

--- sample_files/main.dart
+++ sample_files/main.dart
@@ -1,9 +1,10 @@
-import 'package:capture_flutter/capture_flutter.dart';
 import 'package:flutter/material.dart';
 import "dart:convert";
 import "dart:async";
 import 'package:flutter/services.dart';
 import 'package:capture_flutter/capture_flutter_method_channel.dart';
+import 'dart:typed_data';
+import 'package:capture_flutter/CaptureEnums.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,8 +16,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Uint8List? firstCapturedImage;
+  Uint8List? firstTransformedImage;
+  Uint8List? secondCapturedImage;
+  Uint8List? secondTransformedImage;
+
   Future<void> scan() async {
     var results = await MethodChannelCaptureFlutter.scanWithCamera();
+
+    if (results?.completnessStatus != CompletnessStatus.Empty) {
+      setState(() {
+        firstCapturedImage =
+            base64Decode(getImage(results?.firstCapture?.capturedImage));
+        firstTransformedImage =
+            base64Decode(getImage(results?.firstCapture?.transformedImage));
+        secondCapturedImage =
+            base64Decode(getImage(results?.secondCapture?.capturedImage));
+        secondTransformedImage =
+            base64Decode(getImage(results?.secondCapture?.transformedImage));
+      });
+    }
+  }
+
+  String getImage(String? base64Image) {
+    if (base64Image != null) {
+      return base64Image;
+    }
+    return "";
   }
 
   @override
@@ -28,20 +54,34 @@ class _MyAppState extends State<MyApp> {
         ),
         body: SingleChildScrollView(
           padding: EdgeInsets.all(16.0),
-          child: Builder(
-            builder: (BuildContext context) {
-              return Column(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 16.0),
-                    child: ElevatedButton(
-                      onPressed: () => scan(),
-                      child: Text("Scan with camera"),
-                    ),
+          child: Column(
+            children: <Widget>[
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: 16.0),
+                  child: ElevatedButton(
+                    onPressed: scan,
+                    child: Text("Scan with camera"),
                   ),
-                ],
-              );
-            },
+                ),
+              ),
+              if (firstCapturedImage != null) ...[
+                Text("First Capture - Original"),
+                Image.memory(firstCapturedImage!),
+              ],
+              if (firstTransformedImage != null) ...[
+                Text("First Capture - Transformed"),
+                Image.memory(firstTransformedImage!),
+              ],
+              if (secondCapturedImage != null) ...[
+                Text("Second Capture - Original"),
+                Image.memory(secondCapturedImage!),
+              ],
+              if (secondTransformedImage != null) ...[
+                Text("Second Capture - Transformed"),
+                Image.memory(secondTransformedImage!),
+              ],
+            ],
           ),
         ),
       ),
